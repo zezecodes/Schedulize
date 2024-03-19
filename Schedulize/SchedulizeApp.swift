c155546223d34d12a49c00c834c6ba4051d666ca
Aaron Djangmah <djangsiness01@gmail.com>
2024-03-19 18:26:42 +0000

    Initial Commit

---
+++ Schedulize/SchedulizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulizeApp.swift
+//  Schedulize
+//
+//  Created by Aaron Djangmah on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SchedulizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
